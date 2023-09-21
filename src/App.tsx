83751778f79563a4aa5ffaade939b552facdf4b5
dianna-SE <diannapham.se@gmail.com>
2023-09-21 00:23:58 -0700

    committing to back up expanded display

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, KeyboardEvent } from 'react';
 import "./App.css";
-import { IoIosArrowDown } from "react-icons/io";
 
 
 function App() {
-  const [currentInput, setCurrentInput] = useState("");
+  const [currentInput, setCurrentInput] = useState("0");
   const [isExpanded, setIsExpanded] = useState(false);
 
 
@@ -16,10 +15,10 @@ function App() {
       { display: "+/-", value: "+/-" }
     ],
     [
-      { display: "1", value: "1" },
-      { display: "2", value: "2" },
-      { display: "3", value: "3" },
-      { display: "/", value: "/" }
+      { display: "7", value: "7" },
+      { display: "8", value: "8" },
+      { display: "9", value: "9" },
+      { display: "+", value: "+" }
     ],
     [
       { display: "4", value: "4" },
@@ -28,15 +27,15 @@ function App() {
       { display: "x", value: "x" }
     ],
     [
-      { display: "7", value: "7" },
-      { display: "8", value: "8" },
-      { display: "9", value: "9" },
-      { display: "+", value: "+" }
+      { display: "1", value: "1" },
+      { display: "2", value: "2" },
+      { display: "3", value: "3" },
+      { display: "/", value: "/" }
     ],
     [
+      { display: "0", value: "0" },
       { display: ".", value: "." },
       { display: "^", value: "^" },
-      { display: "0", value: "0" },
       { display: "-", value: "-" }
     ]
   ];
@@ -44,36 +43,63 @@ function App() {
   // displays additional buttons when expanding calculator
   const expandedDisplay = [
     [
+      { display: "1/x", value: "1/(" },
+      { display: "√2", value: "sqrt(2)" },
+      { display: "√3", value: "sqrt(3)" },
+      { display: "log", value: "log(" },
+    ],
+    [
+      { display: "ln", value: "ln(" },
+      { display: "log10", value: "log10(" },
+      { display: "rad", value: "rad" },
+      { display: "e", value: "e" },
+    ],
+    [
       { display: "sin", value: "sin(" },
       { display: "cos", value: "cos(" },
       { display: "tan", value: "tan(" },
-      { display: "log", value: "log(" }
+      { display: "π", value: "π" }
     ],
     [
-      { display: "asin", value: "asin(" },
-      { display: "acos", value: "acos(" },
-      { display: "atan", value: "atan(" },
-      { display: "ln", value: "ln(" }
-    ],
-    [
-      { display: "π", value: "π" },
-      { display: "e", value: "e" },
-      { display: "%", value: "%" },
-      { display: "!", value: "!" }
-    ],
-    [
-      { display: "√", value: "√(" },
-      { display: "x²", value: "^2" },
-      { display: "1/x", value: "1/(" },
-      { display: "|x|", value: "abs(" }
+      { display: "sinh", value: "sinh(" },
+      { display: "cosh", value: "cosh(" },
+      { display: "tanh", value: "tanh(" },
+      { display: "%", value: "%" }
     ]
-  ];
+  ];  
+
+
+  // Handles calculations through key presses
+  const handleKeyDown = (event: KeyboardEvent) => {
+    const keyToValueMap: { [key: string]: string } = {
+      '*': 'x',
+      'Enter': '=',
+      'Escape': 'AC',
+      'Backspace': 'DEL'
+      // ... any other key conversions ...
+    };
+
+    const value = keyToValueMap[event.key] || event.key;
+
+    // Check if value is present in buttonConfig or expandedDisplay, or is a valid character for the calculator
+    const isValuePresent = buttonConfig.flat().some(btn => btn.value === value) || expandedDisplay.flat().some(btn => btn.value === value);
+    if (isValuePresent || /[0-9+\-*/().^]/.test(value)) {
+      handleButtonClick(value);
+    }
+};
   
 
   // Handles calculations through clicks
   const handleButtonClick = (value: string) => {
+    const maxLength = 12; // Adjust the maximum length as needed
+
+    // Check if currentInput exceeds the maximum length
+    if (currentInput.length >= maxLength) {
+      return;
+    }
+
     if (value === "AC") {
-      setCurrentInput("");
+      setCurrentInput("0");
     } 
 
     // checks if input is valid and is not zero, appends a sign in front of input
@@ -86,7 +112,7 @@ function App() {
     }
 
     else if (value === "=") {
-      setCurrentInput("Insert Answer");
+      setCurrentInput("0");
     }
 
     else if (value === "<") {
@@ -112,49 +138,65 @@ function App() {
           setCurrentInput(prev => prev + value);
       }
     }
+
+    // Handle numbers
+    else if (/[0-9]/.test(value)) {
+    // Check if currentInput is "0" and replace it with the new number
+    if (currentInput === "0") {
+      setCurrentInput(value);
+    } else {
+      setCurrentInput(prev => prev + value);
+    }
+    }
+
+
+
     else {
       setCurrentInput(prev => prev + value);
     }
+
   };
 
   
 
 return (
   <div className="main">
-    <h1>Calculator App</h1>
+    <h1>Calculator</h1>
     <section className="calculator-body">
-      <input type="text" value={currentInput} id="calculator-input" readOnly />
+      <input type="text" value={currentInput} id="calculator-input" readOnly/>
 
       <div className="calculator-section">
-        {buttonConfig.map(row => (
-          <div>
-            {row.map(button => (
-              <button className="calculator-button" onClick={() => handleButtonClick(button.value)}>
+      {buttonConfig.map((row, rowIndex) => (
+    <div key={`buttonRow-${rowIndex}`}>
+        {row.map((button, btnIndex) => (
+            <button key={`btn-${btnIndex}`} className="calculator-button" onClick={() => handleButtonClick(button.value)}>
                 {button.display}
-              </button>
-            ))}
-          </div>
+            </button>
         ))}
+    </div>
+))}
       </div>
 
-      {/* Expanded Buttons */}
+      {/* displays additional set of buttons when calculator is expanded */}
       {isExpanded && 
         <div className="expanded-section">
-          {expandedDisplay.map(row => (
-            <div>
-              {row.map(button => (
-                <button className="calculator-button" onClick={() => handleButtonClick(button.value)}>
-                  {button.display}
-                </button>
-              ))}
-            </div>
-          ))}
+{expandedDisplay.map((row, rowIndex) => (
+    <div key={`expandedRow-${rowIndex}`}>
+        {row.map((button, btnIndex) => (
+            <button key={`expandedBtn-${btnIndex}`} className="calculator-button" onClick={() => handleButtonClick(button.value)}>
+                {button.display}
+            </button>
+        ))}
+    </div>
+))}
         </div>
       }
 
       <div>
         <button className="calculator-button" onClick={() => handleButtonClick("=")}>=</button>
       </div>
+
+
 
       <div>
         <button className="calculator-button wide-arrow-btn" onClick={() => handleButtonClick("<")}>
