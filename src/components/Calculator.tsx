b127a39f0201e471e92863ed15e2088b5907f13d
dianna-SE <diannapham.se@gmail.com>
2023-09-29 13:32:08 -0700

    removed unused function from React object

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, KeyboardEvent } from 'react';
+import React, { useState } from 'react';
 import Button from './Button'
 import Display from './Display';
 import CalculatorInput from './CalculatorInput';
