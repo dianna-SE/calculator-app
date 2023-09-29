7155e15d6130805d2064df912e16a28ba0f766e2
dianna-SE <diannapham.se@gmail.com>
2023-09-29 13:08:10 -0700

    updated files and feature

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -49,140 +49,6 @@ const Calculator: React.FC = () => {
         { display: "-", value: "-" }
       ]
     ];
-    
-  
-  //   interface EvalResult {
-  //     value: number;
-  //     status: "OK" | "OVERFLOW" | "ERROR";
-  //     message?: string;
-  // }
-
-  //   // Rounding results
-  //   function roundResult(value: number): number {
-  //     return parseFloat(value.toFixed(4));
-  // }
-      
-      
-  //   // Handle addition and subtraction operations
-  //     function evaluateAddSubtract(x: string, y: string, operation: string): EvalResult {
-  //       let result: number;
-      
-  //       switch (operation) {
-  //         case "+":
-  //           result = roundResult(parseFloat(x) + parseFloat(y));
-  //           break;
-  
-  //         case "-":
-  //           result = roundResult(parseFloat(x) - parseFloat(y)); 
-  //           break;
-  
-  //           default:
-  //             return { value: 0, status: "ERROR", message: "Unknown operation" };
-  //       }
-      
-  //       return { value: result, status: "OK" };
-  //     }
-      
-      
-      
-  //     // Handle multiplication and division operations
-  //     function evaluateMultiplyDivide(x: string, y: string, operation: string): EvalResult {
-  //       let result: number;
-        
-      
-  //       switch (operation) {
-  //         case "x":
-  //           result = roundResult(parseFloat(x) * parseFloat(y));
-  //           break;
-  
-  //         case "/":
-  //           if(parseFloat(y) === 0) {  // Handle division by zero
-  //             return { value: NaN, status: "ERROR" };
-  //           }
-  //           result = roundResult(parseFloat(x) / parseFloat(y));
-  //           break;
-  
-  //           default:
-  //             return { value: 0, status: "ERROR", message: "Unknown operation" };
-  //       }
-          
-  //       return { value: result, status: "OK" };
-  //     }
-      
-  
-  
-  //     // Handle exponentiation operations
-  //     function evaluateExponentiation(x: string, y: string, operation: string): EvalResult {
-  //       let result: number;
-    
-  //       switch (operation) {
-  //           case "^":
-  //               result = roundResult(parseFloat(x) ** parseFloat(y)); 
-                
-  //               if (result > Number.MAX_SAFE_INTEGER) {
-  //                 console.log("Precision issues due to large number")
-  //                 return { value: NaN, status: "ERROR", message: "Result exceeds maximum safe integer" };
-  //               }
-  //               break;
-    
-  //               default:
-  //                 return { value: 0, status: "ERROR", message: "Unknown operation" };
-  //       }
-    
-  //       return { value: result, status: "OK" };
-  //   }
-  
-  
-  //   // Handle modulo operations
-  //   function evaluateModulo(x: string, y: string, operation: string): EvalResult {
-  //     let result: number;
-  
-  //     switch (operation) {
-  //         case "%":
-  //             result = roundResult(parseFloat(x) % parseFloat(y)); 
-  //             break;
-  
-  //             default:
-  //               return { value: 0, status: "ERROR", message: "Unknown operation" };
-  //     }
-  
-  //     return { value: result, status: "OK" };
-  // }
-  
-  
-  // // Handle radical operations
-  // function evaluateRadical(x: string, y: string, operation: string): EvalResult {
-  //   let result: number;
-  
-  //   switch (operation) {
-  //     case "√":  
-  //     let multiplier = parseFloat(x) || 1;  
-  //     let radicand = parseFloat(y);
-  
-  //     if (radicand < 0) {
-  //       return { value: NaN, status: "ERROR", message: "Cannot compute negative number" };
-  //     }
-  
-  //     result = roundResult(multiplier * Math.sqrt(radicand));
-  //     break;
-  
-  
-  //       default:
-  //           return { value: 0, status: "ERROR", message: "Unknown operation" }; 
-  //   }
-  
-  //   return { value: result, status: "OK" };
-  // }
-  
-  
-  
-  // function formatResult(value: string): string {
-  //   if (value.length > 18) {
-  //     const numberValue = parseFloat(value);
-  //     return numberValue.toExponential(4); // rounds to 4 places
-  //   }
-  //   return value;
-  // }
   
 
 
@@ -208,27 +74,8 @@ const Calculator: React.FC = () => {
     }
   };
   
-
-  // const computeResult = (x: string, y: string, operation: string): string => {
-  //   switch (operation) {
-  //     case "+":
-  //     case "-":
-  //       return evaluateAddSubtract(x, y, operation).value.toString();
-  //     case "x":
-  //     case "/":
-  //       return evaluateMultiplyDivide(x, y, operation).value.toString();
-  //     case "^":
-  //       return evaluateExponentiation(x, y, operation).value.toString();
-  //     case "%":
-  //       return evaluateModulo(x, y, operation).value.toString();
-  //     case "√":
-  //       return evaluateRadical(x, y, operation).value.toString();
-  //     default:
-  //       return "";
-  //   }
-  // };
   
-  
+  // Processes any input handled using mouse or keyboard clicks
   const processInput = (inputValue: string) => {
     console.log("x: ", x)
     console.log("y: ", y)
@@ -246,8 +93,6 @@ const Calculator: React.FC = () => {
       return;
           
     };
-  
-  
   
     // 1. Clears the input
     if (inputValue === "AC") {
@@ -511,9 +356,6 @@ const Calculator: React.FC = () => {
   };
 
 
-
-
-
     return (
 
     <section className="calculator-body">
@@ -540,7 +382,7 @@ const Calculator: React.FC = () => {
           {row.map((button, btnIndex) => (
                 <Button 
                     key={`btn-${btnIndex}`} 
-                    className="calculator-button"
+                    className="calculator-button no-select"
                     display={button.display} 
                     value={button.value} 
                     onClick={handleButtonClick} 
